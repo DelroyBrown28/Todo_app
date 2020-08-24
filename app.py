198567ed63f354f783fb4fe87c610df7d3b1113d
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-08-24 20:14:24 +0100

    Updating gitignore file

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = list(mongo.db.tasks.find())
+    tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -80,6 +80,7 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
 
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
